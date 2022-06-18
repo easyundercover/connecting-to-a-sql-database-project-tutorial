57ad84abf77b5215bbbdc318bb87f781a20ca05f
easyundercover <belenfpineyro@gmail.com>
2022-06-18 20:49:58 +0000

    ex 180622

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 
-
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
+import psycopg2
 import pandas as pd
 import numpy as np
 import sqlalchemy as db 
@@ -15,12 +15,12 @@ DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_DATABASE = os.getenv('DB_DATABASE')
 DB_NAME = os.getenv('DB_NAME')
-#Esto no es estrictamente necesario pero es una buena práctica
+#Esto no es estrictamente necesario pero es una buena práctica para no publicar credenciales
 
 print(DB_HOST)
 
 def connect():
-    global engine # this allows us to use a global variable called engine
+    global engine # this allows us to use a global variable called engine 
     # A "connection string" is basically a string that contains all the databse credentials together
     connection_string = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}?autocommit=true"
     print("Starting the connection...")
@@ -71,43 +71,16 @@ engine.execute("""CREATE TABLE book_authors (
 );
 """)
 
+
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-engine.execute("""INSERT INTO publishers(publisher_id,name) values (1,'O Reilly Media');
-INSERT INTO publishers(publisher_id,name) values (2,'A Book Apart');
-INSERT INTO publishers(publisher_id,name) values (3,'A K PETERS');
-INSERT INTO publishers(publisher_id,name) values (4,'Academic Press');
-INSERT INTO publishers(publisher_id,name) values (5,'Addison Wesley');
-INSERT INTO publishers(publisher_id,name) values (6,'Albert&Sweigart');
-INSERT INTO publishers(publisher_id,name) values (7,'Alfred A. Knopf');
-""")
 
-engine.execute("""INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (1,'Merritt',null,'Eric');
-INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (2,'Linda',null,'Mui');
-INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (3,'Alecos',null,'Papadatos');
-INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (4,'Anthony','Molinaro');
-INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (5,'David',null,'Cronin');
-INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (6,'Richard',null,'Blum');
-INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (7,'Yuval','Noah','Harari');
-INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (8,'Paul',null,'Albitz');
-""")
+engine.execute("""INSERT INTO publishers(publisher_id,name) VALUES (1,'O Reilly Media'),(2,'A Book Apart'),(3,'A K PETERS'),(4,'Academic Press'),(5,'Addison Wesley'),(6,'Albert&Sweigart'),(7,'Alfred A. Knopf');""")
 
-engine.execute("""insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (1,'Lean Software Development: An Agile Toolkit',240,4.17,'9780320000000','2003-05-18',5);
-insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (2,'Facing the Intelligence Explosion',91,3.87,null,'2013-02-01',7);
-insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (3,'Scala in Action',419,3.74,'9781940000000','2013-04-10',1);
-insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (4,'Patterns of Software: Tales from the Software Community',256,3.84,'9780200000000','1996-08-15',1);
-insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (5,'Anatomy Of LISP',446,4.43,'9780070000000','1978-01-01',3);
-insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (6,'Computing machinery and intelligence',24,4.17,null,'2009-03-22',4);
-insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (7,'XML: Visual QuickStart Guide',269,3.66,'9780320000000','2009-01-01',5);
-insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (8,'SQL Cookbook',595,3.95,'9780600000000','2005-12-01',7);
-insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (9,'The Apollo Guidance Computer: Architecture And Operation (Springer Praxis Books / Space Exploration)',439,4.29,'9781440000000','2010-07-01',6);
-insert into books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) values (10,'Minds and Computers: An Introduction to the Philosophy of Artificial Intelligence',222,3.54,'9780750000000','2007-02-13',7);
-""")
+engine.execute("""INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (1,'Merritt',null,'Eric'),(2,'Linda',null,'Mui'),(3,'Alecos',null,'Papadatos'),(4,'Anthony', null, 'Molinaro'),(5,'David',null,'Cronin'),(6,'Richard',null,'Blum'),(7,'Yuval','Noah','Harari'),(8,'Paul',null,'Albitz');""")
 
-engine.execute("""INSERT INTO book_authors (book_id, author_id) values (8,4);
-INSERT INTO book_authors (book_id, author_id) values (8,4);
-INSERT INTO book_authors (book_id, author_id) values (8,4);
-INSERT INTO book_authors (book_id, author_id) values (8,4);
-""")
+engine.execute("""INSERT INTO books (book_id,title,total_pages,rating,isbn,published_date,publisher_id) VALUES (1,'Lean Software Development: An Agile Toolkit',240,4.17,'9780320000000','2003-05-18',5),(2,'Facing the Intelligence Explosion',91,3.87,null,'2013-02-01',7),(3,'Scala in Action',419,3.74,'9781940000000','2013-04-10',1),(4,'Patterns of Software: Tales from the Software Community',256,3.84,'9780200000000','1996-08-15',1),(5,'Anatomy Of LISP',446,4.43,'9780070000000','1978-01-01',3),(6,'Computing machinery and intelligence',24,4.17,null,'2009-03-22',4),(7,'XML: Visual QuickStart Guide',269,3.66,'9780320000000','2009-01-01',5),(8,'SQL Cookbook',595,3.95,'9780600000000','2005-12-01',7),(9,'The Apollo Guidance Computer: Architecture And Operation (Springer Praxis Books / Space Exploration)',439,4.29,'9781440000000','2010-07-01',6),(10,'Minds and Computers: An Introduction to the Philosophy of Artificial Intelligence',222,3.54,'9780750000000','2007-02-13',7);""")
+
+engine.execute("""INSERT INTO book_authors (book_id, author_id) VALUES (8,4),(1,2),(9,5),(3,1);""")
 
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
 df = pd.read_sql("SELECT * from publishers;", engine)
